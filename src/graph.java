4e4fff62932a7ab7e8b060b2336c6e9800f600c1
Kylie Pusch <kylie.pusch@gmail.com>
2019-12-10 14:28:04 +0100

    I added a graph file as test

---
+++ src/graph.java
@@ -0,0 +1,8 @@
+public class graph {
+
+    public static void main(String[] args) {
+        String test = "That’s it! You people have stood in my way long enough. I’m going to clown college!";
+        System.out.println(test.length());
+
+    }
+}
