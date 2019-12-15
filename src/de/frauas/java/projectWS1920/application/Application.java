43a6758f8a50bc0c4104d30aff333ffc4b8e0f5d
Kylie Pusch <kylie.pusch@gmail.com>
2019-12-15 17:21:42 +0100

    Dijkstra Implementation, changed mock data

--- src/de/frauas/java/projectWS1920/application/Application.java
+++ src/de/frauas/java/projectWS1920/application/Application.java
@@ -1,6 +1,7 @@
 package de.frauas.java.projectWS1920.application;
 
 import de.frauas.java.projectWS1920.models.Graph;
+import de.frauas.java.projectWS1920.models.Node;
 
 public class Application {
 
@@ -9,6 +10,13 @@ public class Application {
         // load mock data to test before we finish parsing the graph
         Graph mockGraph = MockData.createMockGraph();
         System.out.println("Node 1's neighbours: " + mockGraph.getNodes().get(0).getAdjacentNodes());
+        // test the dijkstra implementation
+        Node testOriginNode = mockGraph.getNodeById(1);
+        mockGraph.calculateShortestPathsFrom(testOriginNode);
+        for (Node node : mockGraph.getNodes()) {
+            System.out.println("Shortest path from " + testOriginNode.getNodeId() + " to " + node.getNodeId() + " is " +
+                    testOriginNode.getShortestPathTo(node));
+        }
 
         // 1. output number of nodes
         System.out.println("Number of nodes: " + mockGraph.getNodes().size());
